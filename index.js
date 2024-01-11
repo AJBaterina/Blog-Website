c5a0a5687cbf37bfd470b7d952a2bbfc90fd12c7
AJBaterina <104185594+AJBaterina@users.noreply.github.com>
2024-01-12 00:03:18 +0800

    Home page

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyparser from "body-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+   res.render("partial/navigator.ejs");
+ });
+
+ app.listen(port, () => {
+   console.log(`Listening on port ${port}`);
+ });
