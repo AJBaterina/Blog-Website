5e65ae3a219f2062213f67fd8bac5f137b042966
AJBaterina <104185594+AJBaterina@users.noreply.github.com>
2024-01-12 15:39:03 +0800

    Modifying the UI

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyparser from "body-parser";
 import bodyParser from "body-parser";
+import serveIndex from "serve-index";
 
 const app = express();
 const port = 3000;
